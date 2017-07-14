5ec6a5ac6e18a70c8b448be40fd8c7ad5808368c
Mike Street <mike@liquidlight.co.uk>
2017-07-14 12:59:16 +0100

    BE: Further config abstraction, config merges config skel

--- config.skel.php
+++ config.skel.php
@@ -1,5 +1,16 @@
-<?php 
+<?php
 
 	return array(
-		'auth_token' => ''
+		'auth_token' => '',
+
+		'trigger_word' => '!tea',
+		'responses' => array(
+			"It's about time {{USER}} put the kettle on - off you trot!",
+			"Pop the kettle on {{USER}} - it's your turn to make a cuppa",
+			"Who wants a drink? {{USER}} is heading to the kitchen to make one",
+			"Coffee? Tea? Sugar? Peppermint Tea? Green Tea? Get your orders in as {{USER}} is making a round",
+			"That's very nice of {{USER}} to make a round of tea!",
+			"Mine is milk 2 sugars please {{USER}} - what about everyone else?",
+			"The tea maker is... {{USER}}! Get brewing."
+		)
 	);
--- tea.php
+++ tea.php
@@ -1,23 +1,14 @@
 <?php
-	
-	$config = include(__DIR__ . '/config.php');
+	// Include slack library from https://github.com/10w042/slack-api
+	include(__DIR__ . '/Slack.php');
 
-	$trigger_word = '!tea';
-	$responses = array(
-		"It's about time {{USER}} put the kettle on - off you trot!",
-		"Pop the kettle on {{USER}} - it's your turn to make a cuppa",
-		"Who wants a drink? {{USER}} is heading to the kitchen to make one",
-		"Coffee? Tea? Sugar? Peppermint Tea? Green Tea? Get your orders in as {{USER}} is making a round",
-		"That's very nice of {{USER}} to make a round of tea!",
-		"Mine is milk 2 sugars please {{USER}} - what about everyone else?",
-		"The tea maker is... {{USER}}! Get brewing."
+	$config = array_merge(
+		include(__DIR__ . '/config.skel.php'),
+		include(__DIR__ . '/config.php')
 	);
 
-	// Include slack library from https://github.com/10w042/slack-api
-	include 'Slack.php';
-
 	// Remove our keyword from the text to extract name to exclude
-	$exclude = str_replace($trigger_word . ' ', '', $_POST['text']);
+	$exclude = str_replace($config['trigger_word'] . ' ', '', $_POST['text']);
 
 	// Connect to Slack
 	// Use authentication token found here: https://api.slack.com/
@@ -35,7 +26,7 @@
 		$userData = $Slack->call('users.info', array('user' => $m));
 		// Check to see if the user is online before adding them to list of brewers
 		$presence = $Slack->call('users.getPresence', array('user' => $m));
-		
+
 		$user = $userData['user'];
 
 		// If there is an exclude, check to see if it matches a user real name (lowercase)
@@ -49,18 +40,12 @@
 			}
 	}
 
-	// Shuffle shuffle shuffle the arrays
-	function pickOne($array) {
-		shuffle($array);
-		return $array[mt_rand(0, (count($array) - 1))];
-	}
-
 	// Get a random user from the array
 	$user = pickOne($teaMakers);
 
 	// SEND OUT THE JSON!! Enjoy your brew
 	header('Content-Type: application/json');
 	echo json_encode(array(
-		'text' => str_replace('{{USER}}', '<@' . $user['id'] . '>', pickOne($responses))
+		'text' => str_replace('{{USER}}', '<@' . $user['id'] . '>', pickOne($config['responses']))
 	));
 
